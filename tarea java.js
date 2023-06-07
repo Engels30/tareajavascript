35492939e1ae1ee399d980477fde8625c0ce2253
Engels30 <132589982+Engels30@users.noreply.github.com>
2023-06-07 09:00:28 -0600

    Add files via upload

---
+++ tarea java.js
@@ -0,0 +1,10 @@
+let contactos = [
+  { nombre: "Juan Pérez" },
+  { nombre: "María García" },
+  { nombre: "Pedro González" }
+];
+
+// Mostrar la lista de contactos
+for (let contacto of contactos) {
+  console.log(contacto.nombre);
+}
